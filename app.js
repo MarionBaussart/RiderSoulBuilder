7c08c268908560c2138b0c304ca6b0f9bb8aff78
MarionBaussart <marion.baussart@gmail.com>
2022-06-29 09:42:25 +0000

    update

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
 
 // statics ressources
 app.use(express.static('./front/pictures'));
 app.use(express.static('./front/styles'));
-
-// //example that give the date of the request
-// app.use((req, res, next) => {
-//     console.log("request succeed : " + Date().toString());
-//     next(); // pass to the next function middleware
-// });
-
-// information about request in the terminal
-app.use(morgan("dev"));
 
 // routes
 app.get('/home', (req, res) => {
@@ -28,13 +18,10 @@ app.get('/', (req, res) => {
     res.status(300).redirect("/home");
 });
 
-// route not found
 app.use((req, res) => {
-    res.status(404).sendFile("./front/error.html", {root: __dirname});
+    res.status(404).sendFile("./front/error.html", {root : __dirname});
 });
 
-//listening
-app.listen(5001, () => {
-    console.log("Waiting for requests at port 5001")
-});
-console.log("Error when server created");
+// listening on port 5001
+app.listen(5001);
+console.log('Waiting for request at port 5001');
